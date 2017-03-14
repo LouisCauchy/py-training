561155f469ad87af40c06a5e4d67400ee70b86ae
Liubov Hubska <chamberikjorik@gmail.com>
2017-03-14 14:00:37 +0200

    functions

--- csv2html.py
+++ csv2html.py
@@ -2,16 +2,21 @@
 
 import sys
 
-file = open("data/sample.csv").read().replace('"','').replace('\n',',').split(',')
-words = []
-nums = []
-for i in file:
-    if i.isdecimal():
-        nums.append(i)
-    else:
-        words.append(i)
-words.pop(4)
-words.pop(5)
+def parsing():
+    file = open("data/sample.csv").read().replace('"','').replace('\n',',').split(',')
+    words = []
+    nums = []
+    for i in file:
+        if i.isdecimal():
+            nums.append(i)
+        else:
+            words.append(i)
+    words.pop(4)
+    words.pop(5)
+    print(words, nums)
+
+def making_html():
+    
 
    
             
