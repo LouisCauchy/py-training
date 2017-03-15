fd17b3b5659806048abd63143e951947ecacfdea
Liubov Hubska <chamberikjorik@gmail.com>
2017-03-15 23:19:07 +0200

    last modified

---
+++ count-unique.py
@@ -0,0 +1,14 @@
+def process(lst):
+	dct = {}
+	for i in lst:
+		if i in dct:
+			dct[i] += 1
+		else:
+			dct[i] = 1
+	return dct
+
+def main():
+	lst = input("Enter values:").split(',')
+	print(process(lst))
+
+main()
